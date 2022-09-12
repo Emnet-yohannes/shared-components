15e07c373d8070d80f68781928b29d63bc77a053
emnet yohannes <emnet.yohan@gmail.com>
2022-09-12 16:19:43 +0300

    i18n added

---
+++ whitelabel/i18n/i18n-instance.tsx
@@ -0,0 +1,30 @@
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+import  en from './lng/en.json'
+import  de from './lng/de.json'
+import  fr from './lng/fr.json'
+i18n
+    .use(LanguageDetector)
+    .use(initReactI18next) // passes i18n down to react-i18next
+    .init({
+      resources:
+        {
+              en: {
+                translation: en
+              },
+              de: {
+                translation: de
+              },
+              fr:{
+                translation:fr
+              }
+            },
+      lng: "en",
+      fallbackLng: "en",
+
+      interpolation: {
+        escapeValue: false,
+      },
+    });
+export default i18n;
