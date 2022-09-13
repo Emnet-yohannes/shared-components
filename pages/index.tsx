ed8161c54a8e4a96be7c3c48a3c6001a2cb66e8e
emnet yohannes <emnet.yohan@gmail.com>
2022-09-13 12:13:25 +0300

    tw config configured

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import AssetCardHeader from "../whitelabel/components/AssetCard/AssetCardHeader";
+import AssetCardAction from "../whitelabel/components/AssetCard/AssetCardAction";
+import AssetCardMedia from "../whitelabel/components/AssetCard/AssetCardMedia";
+import AssetContent from "../whitelabel/components/AssetCard/AssetCardContent";
+import AssetContainer from "../whitelabel/components/AssetCard/AssetCardContainer";
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
@@ -12,46 +16,38 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      {/* <main className={styles.main}> */}
+      <AssetContainer language="en" classes="w-full w-[60%] text-white">
+        <div className="">
+          <AssetCardMedia isNextjs={true}  imageUrl={"https://arweave.net/dm4ohGlzQmUYZ8-NfQ85YAf7TshRn0rDWo1KC58zV48"} />
+          <div className="">
+            <div className="">
+              <AssetCardHeader
+                name={`name ${1}`}
+                classes="text-30px,"
+                iconClassName="text-white fill-white h-[20px] w-[20px]"
+              />
+            </div>
+            <div className="">
+              <AssetContent
+                popularity={12}
+                showRarityPrice={true}
+                showPopularity={true}
+                showTotalBids={true}
+              />
+            </div>
+            <div className="">
+              <AssetCardAction
+                iconClassName="!fill-red-500"
+                classes="text-white "
+                showFavorite={true}
+              />
+            </div>
+          </div>
         </div>
-      </main>
+      </AssetContainer>
+
+      {/* </main> */}
 
       <footer className={styles.footer}>
         <a
@@ -59,14 +55,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
