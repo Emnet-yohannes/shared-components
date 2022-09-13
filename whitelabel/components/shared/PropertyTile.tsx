cbdc57850080e743403054895aac1a504dd594cf
emnet yohannes <emnet.yohan@gmail.com>
2022-09-13 11:11:41 +0300

    postcss added for tailwind css

--- whitelabel/components/shared/PropertyTile.tsx
+++ whitelabel/components/shared/PropertyTile.tsx
@@ -10,12 +10,13 @@ interface Props {
   
   export default function PropertyTile(props: Props) {
     return (
-      <div style={props?.customStyle} className={clsxm(`rounded-[5px] bg-black px-6 py-4 text-center capitalize `,props?.classes)}>
+      <div style={props?.customStyle} className={clsxm(`rounded-[5px] bg-white px-6 py-4 text-center capitalize `,props?.classes)}>
         <div className={`text-sm text-[#8E8B8F]`}>{props?.label}</div>
   
         <div className={`text-lg font-extrabold text-[#F6F6F6]`}>{props?.value}</div>
   
-        <div className={`text-sm text-[#8E8B8F]`}>{props?.occurrence.toFixed(2)}&#37; have this trait</div>
+        <div className={`text-sm text-[#8E8B8F]`}>{props?.occurrence.toFixed(2)}
+        &#37; have this trait</div>
       </div>
     );
   }
